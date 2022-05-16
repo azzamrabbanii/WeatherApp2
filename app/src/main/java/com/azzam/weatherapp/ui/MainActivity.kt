62962cfec8c321a535c1fbe6a770c6262d1b6504
Muhammad Azzam <MazzamR08@gmail.com>
2022-05-16 15:05:26 +0700

    initial commit

--- app/src/main/java/com/azzam/weatherapp/ui/MainActivity.kt
+++ app/src/main/java/com/azzam/weatherapp/ui/MainActivity.kt
@@ -2,11 +2,54 @@ package com.azzam.weatherapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.SearchView
+import androidx.lifecycle.ViewModelProvider
 import com.azzam.weatherapp.R
+import com.azzam.weatherapp.databinding.ActivityMainBinding
+import retrofit2.http.Query
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding as ActivityMainBinding
+
+    private var _viewModel: MainViewModel? = null
+    private val viewModel get() = _viewModel as MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        _viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+
+        searchCity()
+        viewModel.getWeatherByCity().observe(this){
+            binding.apply {
+                tvCity.text = it.name
+                tvDegree.text = it.main?.temp.toString()
+            }
+        }
+
+
+    }
+
+    private fun searchCity(){
+        binding.edtSearch.setOnQueryTextListener(
+            object : SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                    query?.let { viewModel.weatherByCity(it)
+                    }
+                    return true
+                }
+
+                override fun onQueryTextChange(p0: String?): Boolean {
+                    return false
+                }
+
+            }
+        )
+
     }
 }
--- app/src/main/java/com/azzam/weatherapp/ui/MainViewModel.kt
+++ app/src/main/java/com/azzam/weatherapp/ui/MainViewModel.kt
@@ -1,5 +1,8 @@
 package com.azzam.weatherapp.ui
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.azzam.weatherapp.data.WeatherResponse
 import com.azzam.weatherapp.network.ApiConfig
@@ -8,19 +11,26 @@ import retrofit2.Callback
 import retrofit2.Response
 
 class MainViewModel : ViewModel() {
+
+    val weatherByCity = MutableLiveData<WeatherResponse>()
+
     fun weatherByCity(city: String) {
         ApiConfig.getApiService().weatherByCity(city).enqueue(object : Callback<WeatherResponse> {
             override fun onResponse(
                 call: Call<WeatherResponse>,
                 response: Response<WeatherResponse>
             ) {
-                TODO("Not yet implemented")
+                if (response.isSuccessful) { weatherByCity.postValue(response.body())
+                }
             }
 
             override fun onFailure(call: Call<WeatherResponse>, t: Throwable) {
-                TODO("Not yet implemented")
+                Log.e("failure", t.message.toString())
             }
 
         } )
     }
+
+    fun getWeatherByCity() : LiveData<WeatherResponse> = weatherByCity
+
 }
